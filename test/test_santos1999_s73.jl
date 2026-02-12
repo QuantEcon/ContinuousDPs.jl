6964d01a150c66ce283760f574f7478aa5638ab7
mnshkw <mao.nishikawa24@gmail.com>
2026-02-13 03:52:26 +0900

    TEST: fix and decrease grid size to assure convergence

--- test/test_santos1999_s73.jl
+++ test/test_santos1999_s73.jl
@@ -166,7 +166,7 @@ end
         params = default_params()
 
         # Grid setup
-        nk, nlogz = 5, 5
+        nk, nlogz = 3, 3
 
         # Shock discretization (Gauss-Hermite quadrature)
         n_shocks = 7
@@ -212,7 +212,7 @@ end
                     k_prime_star_on_S = policy.(k_nodes, logz_nodes)
                     
                     # Solve DP
-                    res = solve(cdp, method, max_iter=500, tol=sqrt(eps()), verbose=0)
+                    res = solve(cdp, method, max_iter=500, tol=sqrt(eps()))#, verbose=0)
                     results[test_name] = res
                     l_hat = vec(res.X)
 
