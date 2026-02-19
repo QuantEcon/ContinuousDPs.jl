fae9fea06592d778788b22949581f4e64a29aed9
mnshkw <mao.nishikawa24@gmail.com>
2026-02-20 03:39:37 +0900

    align Santos (1999) Sec. 7.3 test with Table 16 (linear only)
    - Refactor test configurations to closely follow Santos (1999)
    
    - Add error assertion tests against the benchmark analytical solution
    
    - Restrict interpolation method to Linear only, mathing Santos (1999) Table 16 (removed multiple basis testing).

--- test/test_santos1999_s73.jl
+++ test/test_santos1999_s73.jl
@@ -4,10 +4,6 @@
 # Model parameters (as in Santos, 1999, Sec. 7.3)
 beta, lambda, A, alpha, delta, rho, sigma_epsilon = 0.95, 1 / 3, 10.0, 0.34, 1.0, 0.90, 0.008
 
-# Mesh size (as in Santos, 1999, Sec. 7.3)
-# Exclude (500, 33) due to high computational cost
-mesh_settings = ((43, 3), (143, 9))
-
 # State domains (as in Santos, 1999, Sec. 7.3)
 logz_min, logz_max = -0.32, 0.32
 k_min, k_max = 0.10, 10.0
@@ -15,17 +11,15 @@ k_min, k_max = 0.10, 10.0
 # For numerical stability
 x_lb(s), x_ub(s) = 1e-10, 1 - 1e-10
 
-# Grid and bases setup
-nk, nlogz = 3, 3
-deg_k, deg_z = 3, 1
-dk = min(deg_k, nk - 1)
-dz = min(deg_z, nlogz - 1)
-breaks_k = nk - (dk - 1)
-breaks_z = nlogz - (dz - 1)
-bases = Basis[]
-push!(bases, Basis(LinParams(nk, k_min, k_max), LinParams(nlogz, logz_min, logz_max)))
-push!(bases, Basis(ChebParams(nk, k_min, k_max), ChebParams(nlogz, logz_min, logz_max)))
-push!(bases, Basis(SplineParams(breaks_k, k_min, k_max, dk), SplineParams(breaks_z, logz_min, logz_max, dz)))
+# Mesh size (as in Santos, 1999, Sec. 7.3)
+# Exclude (500, 33) due to high computational cost
+mesh_settings = ((43, 3), (143, 9))
+function mesh_size_h(mesh_setting)
+    nk, nlogz = mesh_setting
+    grid_size_k = (k_max - k_min) / (nk - 1)
+    grid_size_logz = (logz_max - logz_min) / (nlogz - 1)
+    return sqrt(grid_size_k^2 + grid_size_logz^2)
+end
 
 # Model functions
 # Production and Santos (7.4)-style mapping: given leisure l -> (c, k')
@@ -75,7 +69,7 @@ B = const_term / (1 - beta)
 v_star(k, logz) = B + C * log(k) + D * logz
 
 @testset "Santos (1999) Sec. 7.3: stochastic growth w/ leisure (2D state, 1D control) benchmarks" begin
-    # Test: Solutions with multiple methods and interpolations
+    # Test: Linear basis with VFI and PFI should match analytical solution on interpolation nodes within tolerances based on Santos (1999) Sec. 7.3
     # Shock discretization (Gauss-Hermite quadrature)
     n_shocks = 7
     shocks, weights = qnwnorm(n_shocks, 0.0, sigma_epsilon^2)
@@ -84,7 +78,18 @@ v_star(k, logz) = B + C * log(k) + D * logz
     methods = [VFI, PFI]
 
     for method in methods
-        for basis in bases
+        for mesh_setting in mesh_settings
+            # Calculate mesh size for tolerance settings
+            h = mesh_size_h(mesh_setting)
+
+            # Tolerances based on Santos (1999) Table 16: observed constants are approximately 0.36
+            policy_tol = 0.4 * h
+            # Tol based on Santos (1999) Table 16: measured upper bounds are approximately 24
+            value_tol = 24 * h^2
+
+            # Build basis
+            basis = Basis(LinParams(mesh_setting[1], k_min, k_max), LinParams(mesh_setting[2], logz_min, logz_max))
+
             # Build DP
             cdp = ContinuousDP(f, g, beta, shocks, weights, x_lb, x_ub, basis)
 
@@ -93,19 +98,21 @@ v_star(k, logz) = B + C * log(k) + D * logz
             k_nodes = @view S[:, 1]
             logz_nodes = @view S[:, 2]
             v_star_on_S = v_star.(k_nodes, logz_nodes)
+            k_prime_star_on_S = policy.(k_nodes, logz_nodes)
 
             # Solve DP
             res = solve(cdp, method, max_iter=500, tol=sqrt(eps()), verbose=0)
             l_hat = vec(res.X)
+            k_hat = kprime_from_l.(k_nodes, exp.(logz_nodes), l_hat)
 
             # Convergence tests
             @test res.converged
 
             # Policy function benchmark check
-            println("max |l_hat - l_star| = ", maximum(abs.(l_hat .- l_star)))
+            @test maximum(abs, k_hat .- k_prime_star_on_S) <= policy_tol
 
             # Value function benchmark check
-            println("max |V_hat - V_star| = ", maximum(abs.(res.V .- v_star_on_S)))
+            @test maximum(abs, res.V .- v_star_on_S) <= value_tol
         end
     end
 end
