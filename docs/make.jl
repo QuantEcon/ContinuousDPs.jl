04565dc2fb77402514e1df215125098487c94606
mnshkw <mao.nishikawa24@gmail.com>
2026-03-27 12:14:31 +0900

    docs: stop rewriting docs/src/index.md during docs build

--- docs/make.jl
+++ docs/make.jl
@@ -4,16 +4,6 @@ using Documenter
 
 DocMeta.setdocmeta!(ContinuousDPs, :DocTestSetup, :(using ContinuousDPs); recursive=true)
 
-# Sync README.md to src/index.md for the documentation
-function sync_readme_to_index()
-    src = joinpath(@__DIR__, "..", "README.md")
-    dst = joinpath(@__DIR__, "src", "index.md")
-    text = read(src, String)
-    write(dst, text)
-end
-
-sync_readme_to_index()
-
 makedocs(;
     modules=[ContinuousDPs],
     authors="QuantEcon",
